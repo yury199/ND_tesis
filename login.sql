9e1c76fac3cd9e60a88def7e4aefbfd57e9b9404
yury199 <yuryjessenia@gmail.com>
2023-05-28 06:23:49 -0500

    labase de datos,es más rapido poner un usuario como el mio

---
+++ login.sql
@@ -0,0 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-05-2023 a las 13:22:01
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historietas`
+--
+
+CREATE TABLE `historietas` (
+  `id` int(11) NOT NULL,
+  `imgUrl` varchar(555) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `titlestory` varchar(100) NOT NULL,
+  `genero` varchar(50) NOT NULL,
+  `descripcion` varchar(305) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `id_historieta` int(100) NOT NULL,
+  `parent` int(11) DEFAULT NULL,
+  `imgUrl` varchar(500) NOT NULL,
+  `usuario` varchar(30) DEFAULT NULL,
+  `titlestory` varchar(200) DEFAULT NULL,
+  `Descripcion` varchar(150) DEFAULT NULL,
+  `emocion` set('feliz','neutral','triste','sorprendido','enojado','asqueado','asustado','N/A') NOT NULL DEFAULT 'N/A',
+  `genero` enum('aventuras','fantasía','ciencia ficción','superhéroes','humor','misterio','históricas') NOT NULL,
+  `climax` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombreusuario` varchar(100) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `contrasena` varchar(50) NOT NULL,
+  `correo` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombreusuario`, `nombre`, `contrasena`, `correo`) VALUES
+(11, 'Luisa123', 'luiosa', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'luisa@gmail.com'),
+(12, 'papa', 'luisa la bonita', 'b5761861cee8613452bfda9172977cc96c723c2d', 'papa@gmail.com'),
+(13, 'yuti', 'yuri', 'b5761861cee8613452bfda9172977cc96c723c2d', 'yuri@gmail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `historietas`
+--
+ALTER TABLE `historietas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `historietas`
+--
+ALTER TABLE `historietas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=112;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=649;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
